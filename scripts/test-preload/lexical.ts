1c9882011ffc15a1f5412e958c35b48a7bf4e903
au-re <au.re@live.com>
2026-06-22 15:30:15 +0200

    chore(PS-66): remove lexical test preload

--- scripts/test-preload/lexical.ts
+++
@@ -1,8 +0,0 @@
-// Bun can initialize Lexical peer packages concurrently during tests, which trips
-// @lexical/markdown's circular references. Load the peers first in a stable order.
-import "lexical";
-import "@lexical/utils";
-import "@lexical/list";
-import "@lexical/rich-text";
-import "@lexical/code";
-import "@lexical/link";
